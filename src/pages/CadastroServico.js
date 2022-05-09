0367e5d0331fcc75c21547354778bebaad34f3f2
Nicoly Barros <nicolybarros00@gmail.com>
2022-05-09 17:57:12 -0300

    Iniciado projeto LabeNinjas - dia 1.

---
+++ src/pages/CadastroServico.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import axios from "axios"
+
+
+export default class CadastroServico extends React.Component{
+    render(){
+        return (
+            <div>
+                <h2>Cadastre um novo Job</h2>
+                form
+            </div>
+        )
+    }
+}
---
+++ src/pages/DetalhesServicos.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import axios from "axios"
+
+
+export default class DetalhesServicos extends React.Component{
+    render(){
+        return (
+            <div>
+                sou o detalhes
+            </div>
+        )
+    }
+}
