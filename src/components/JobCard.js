f7566439715f32cb735b65b107efa88bdc04c170
Isabelle Daru Bisoni <isadarub@gmail.com>
2022-05-10 10:54:17 -0300

    Tentando modificar renderização de produtos mapeados e produtos filtrados

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+const converterData = (date) => {
+    const day = date.substring(8, 10)
+    const month = date.substring(5, 7)
+    const year = date.substring(0, 4)
+    return `${day}/${month}/${year}`
+}
+
 
 const JobCard = (props)=> {
     return (
         <div>
             <h4>{props.job.title}</h4>
-            <p>Preço: {props.job.price}</p>
-            <p>Prazo:</p>
+            <p>Preço: R$ {props.job.price},00</p>
+            <p>Prazo: {converterData(props.job.dueDate)}</p>
 
             <button>Detalhes</button>
             <button>Remover job</button>
